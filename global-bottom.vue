3a68a1441231ef429f763bc8535753e0ab4766cb
Tiago Medicci Serrano <tiago.medicci@gmail.com>
2025-07-21 17:19:14 -0300

    Initial commit

---
+++ global-bottom.vue
@@ -0,0 +1,10 @@
+<!--
+ <template>
+  <footer
+    v-if="$nav.currentLayout !== 'cover'"
+    class="absolute bottom-0 left-0 right-0 p-2 pr-4 text-right"
+  >
+    {{ $nav.currentPage }} / {{ $nav.total }}
+  </footer>
+</template>
+ -->
---
+++ global-top.vue
@@ -0,0 +1,15 @@
+<template>
+  <header
+    style="
+      position: fixed;
+      top: 1rem;
+      right: 2rem;
+      width: 200px;
+      opacity: 0.8;
+      pointer-events: none;
+      z-index: 9999;
+    "
+  >
+    <img src="/espressif-logos/Espressif_Standard_Logo_EN_Horizontal_Black.svg" alt="Espressif Logo" style="width: 100%;" />
+  </header>
+</template>
